59f8a0a20a06048f8fcff8c6861c70c759586a8b
Andrea Reinicke <andrea.reinicke@gmx.net>
2023-02-14 20:54:36 +0100

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Andrea Reinicke on 14.02.23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
